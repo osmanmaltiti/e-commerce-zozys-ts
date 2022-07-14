6a9f091a86763aefa1d14a0995b216bdf43aab9d
osmanmaltiti <terabyte7778@gmail.com>
2022-07-13 17:17:23 -1200

    implementing testing

--- zozys-web/components/landingPage/Navbar.tsx
+++ zozys-web/components/landingPage/Navbar.tsx
@@ -1,11 +1,9 @@
 import { Icon } from "@iconify/react";
 import React from "react";
-import { useMedia } from "../../hooks/useMedia";
 import { Links } from "./Links";
 
 export const Navbar = () => {
   const [openMenu, setOpenMenu] = React.useState<boolean>(false);
-  const lg = useMedia("(min-width: 1024px)");
 
   return (
     <nav className="w-full h-[3rem] fixed top-0 flex flex-row justify-between lg:justify-around items-center py-2 px-4 text-white backdrop-blur-sm z-20 bg-[#00000088]">
@@ -13,17 +11,20 @@ export const Navbar = () => {
         <p className="text-3xl font-semibold lg:hidden">Zozys</p>
         <p className="text-2xl hidden lg:inline">ZOZYS ENTERPRISE</p>
       </>
-      <Icon
-        onClick={() => setOpenMenu(!openMenu)}
-        icon="dashicons:menu"
-        className="text-3xl lg:hidden"
-      />
+      <span className="lg:hidden ml-auto">
+        <Icon
+          onClick={() => setOpenMenu(!openMenu)}
+          icon="dashicons:menu"
+          className="text-3xl lg:hidden"
+        />
+      </span>
       <>
-        {lg ? (
+        <span className="hidden lg:inline">
           <Links state="desktop" open={openMenu} />
-        ) : (
+        </span>
+        <span className="lg:hidden">
           <Links state="mobile" open={openMenu} />
-        )}
+        </span>
       </>
     </nav>
   );
